108ed6d20ba7c3df8c4d7e61a1de92e7c7506ceb
Anonyo Hossain <anonyo.hossain@koolicar.com>
2018-11-11 18:18:41 -0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -171,10 +171,10 @@ const Index = () => (
       />
       <Content speed={0.4} offset={1.2} factor={2}>
         <Inner>
-          <Title>Projects</Title>
+          <Title>How it works</Title>
           <ProjectsWrapper>
             <ProjectCard
-              title="Freiheit"
+              title="Tell us about your injury"
               link="https://www.behance.net/gallery/58937147/Freiheit"
               bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
             >
@@ -185,21 +185,21 @@ const Index = () => (
               link="https://www.behance.net/gallery/52915793/Harry-Potter"
               bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
             >
-              I entered the DOCMA 2017 award with this Harry Potter inspired image.
+              We’ll ask you some background questions about your injury and what appointment times are convenient.
             </ProjectCard>
             <ProjectCard
-              title="Tomb Raider"
+              title="We’ll find a physiotherapist match"
               link="https://www.behance.net/gallery/43907099/Tomb-Raider"
               bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
             >
-              Recreation of a Tomb Raider Wallpaper (Fan Art)
+              We’ll match you with a PT and confirm your appointment time.
             </ProjectCard>
             <ProjectCard
-              title="Eagle"
+              title="A Physiotherapist comes to you."
               link="https://www.behance.net/gallery/38068151/Eagle"
               bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
             >
-              A fantasy image manipulation relocating the habitat of wild animals.
+              A PT comes to you to give you an assessment and create a treatment plan.
             </ProjectCard>
           </ProjectsWrapper>
         </Inner>
@@ -252,17 +252,15 @@ const Index = () => (
           <AboutHero>
             <Avatar src={avatar} alt="John Doe" />
             <AboutSub>
-              The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-              Emoji into my speech to better express myself. Winky face.
+              Exercises taught at home are more effective than when taught in a clinical setting.
             </AboutSub>
           </AboutHero>
           <AboutDesc>
-            You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-            every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-            make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The
-            kids want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that
-            all right?
+            Patients save 40 minutes on average per appointment, by eliminating travel and wait.
           </AboutDesc>
+          <AboutSub>
+            Pay the same for an in-home appointment as a trip to the physiotherapist office.
+          </AboutSub>
         </Inner>
       </Content>
       <Divider fill="#23262b" speed={0.2} offset={4}>
